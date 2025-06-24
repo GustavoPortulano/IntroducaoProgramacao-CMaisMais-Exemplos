1252ce88f58c7fde0364bfaedbba71aed16ec516
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-24 17:09:46 -0300

    Primeiro commit pagina34

---
+++ capitulo3-pagina34/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+// 24 de junho de 2025. Terça-feira
+// Tarde fria
+
+// Capítulo 3, página 34: Dados lógicos e comparações
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	bool var1;
+	var1 = true;
+	bool var2;
+	var2 = false;
+	
+	cout << "var1 = " << var1 << endl;
+	cout << "var2 = " << var2 << endl;
+	
+	system("pause");
+	
+	return 0;
+}
