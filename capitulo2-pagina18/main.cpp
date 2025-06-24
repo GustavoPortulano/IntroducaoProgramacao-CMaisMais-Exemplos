9e028afc20185280625e63340cda85abe9f7ce7b
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-23 23:44:38 -0300

    Primeiro commit pagina18

---
+++ capitulo2-pagina18/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*23 de junho de 2025*/
+/*Segunda-feira, noite fria*/
+
+/*Capítulo 2, página 18*/
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	cout << "Olá mundo!" << endl;
+	
+	return 0;
+}
