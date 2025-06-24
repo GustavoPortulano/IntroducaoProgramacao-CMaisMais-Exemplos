73a78f08c6a509fd403d5a488980aba927843222
GustavoPortulano <gfa.portulano@gmail.com>
2025-06-23 23:36:01 -0300

    Segundo commit pagina17

---
+++ capitulo2-pagina17/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*23 de junho de 2025*/
+/*Noite fria*/
+
+/*Exemplo do capítulo 2, pagina 17*/
+
+int main(int argc, char** argv) {
+	
+	std::cout << "Olá mundo!" << std::endl;
+	
+	return 0;
+}
